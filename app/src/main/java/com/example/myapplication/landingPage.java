70b22b2c5e53c0cac1f34aa0774d983ff5f5db92
esoon <p20012522@student.newinti.edu.my>
2021-10-04 23:30:16 +0800

    Search Page Created

--- app/src/main/java/com/example/myapplication/landingPage.java
+++ app/src/main/java/com/example/myapplication/landingPage.java
@@ -9,7 +9,7 @@ import android.text.Spanned;
 import android.text.style.ForegroundColorSpan;
 import android.widget.TextView;
 
-public class LandingPage extends AppCompatActivity {
+public class landingPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
