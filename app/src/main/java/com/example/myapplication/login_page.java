d8c9a78a16e4f9295b354f8fdb117113d851bcb9
Adeline <p20012407@student.newinti.edu.my>
2021-10-10 22:06:51 +0800

    6

---
+++ app/src/main/java/com/example/myapplication/login_page.java
@@ -0,0 +1,42 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class login_page extends ActionBarActivity implements View.OnClickListener{
+     Button buttonBackLanding, buttonForgetPassword, buttonHomePage;
+     EditText editTextTextEmailAddress, editTextTextPassword;
+
+     @Override
+     protected void onCreate(Bundle savedInstanceState){
+          super.onCreate(savedInstanceState);
+          setContentView(R.layout.activity_login);
+
+          editTextTextEmailAddress = (EditText) findViewById(R.id.editTextTextEmailAddress);
+          editTextTextPassword = (EditText) findViewById(R.id.editTextTextPassword);
+          buttonBackLanding = (Button) findViewById(R.id.buttonBackLanding);
+          buttonForgetPassword = (Button) findViewById(R.id.buttonForgetPassword);
+          buttonHomePage = (Button) findViewById(R.id.buttonHomePage);
+
+          buttonBackLanding.setOnClickListener(this);
+          buttonForgetPassword.setOnClickListener(this);
+          buttonHomePage.setOnClickListener(this);
+     }
+
+     @Override
+     public void onClick(View view) {
+
+          switch(view.getId()){
+               case R.id.buttonBackLanding:
+                    Intent backLanding = new Intent(this,landingPage.class);
+                    startActivity(backLanding);
+                    break;
+          }
+
+     }
+
+
+}
