f35b82c8d081c307bd9051bd0fde8cf8cef2e4d0
cheahheng12 <cheah_heng@yahoo.com>
2021-10-11 22:52:27 +0800

    ij

--- app/src/main/java/com/example/myapplication/rice_page.java
+++ app/src/main/java/com/example/myapplication/rice_page.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -32,7 +34,8 @@ import butterknife.BindDimen;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class rice_page extends AppCompatActivity implements IRiceLoadListener {
+public class rice_page extends AppCompatActivity implements IRiceLoadListener, View.OnClickListener {
+    private Button account1,home1,orderHistory1;
 
     @BindView(R.id.riceListRecycler)
     RecyclerView riceListRecycler;
@@ -51,8 +54,22 @@ public class rice_page extends AppCompatActivity implements IRiceLoadListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rice_main_page);
 
+        account1 = findViewById(R.id.account1);
+        account1.setOnClickListener(this);
+
+        home1 = findViewById(R.id.home1);
+        home1.setOnClickListener(this);
+
+        orderHistory1= findViewById(R.id.orderHistory1);
+        orderHistory1.setOnClickListener(this);
+
+        cart1= findViewById(R.id.cart1);
+        cart1.setOnClickListener(this);
+
         init();
         loadRiceFromFirebase();
+
+
     }
 
     private void loadRiceFromFirebase() {
@@ -111,5 +128,28 @@ public class rice_page extends AppCompatActivity implements IRiceLoadListener {
     @Override
     public void onRiceLoadFailed(String message) {
         Snackbar.make(rice_layout, message, Snackbar.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.account1:
+                Intent toLogin = new Intent(this, Account.class);
+                startActivity(toLogin);
+                break;
+            case R.id.home1:
+                Intent toLogin1 = new Intent(this, HomePage.class);
+                startActivity(toLogin1);
+                break;
+            case R.id.orderHistory1:
+                Intent toLogin2 = new Intent(this, orderHistory.class);
+                startActivity(toLogin2);
+                break;
+            case R.id.cart1:
+                Intent toLogin3 = new Intent(this, cart.class);
+                startActivity(toLogin3);
+                break;
+
+        }
     }
 }
