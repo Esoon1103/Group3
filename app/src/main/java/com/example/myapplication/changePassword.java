96168a9f0fa4687801110bc0da33258f2fd9c009
Ong Jia Jun <p20012439@student.newinti.edu.my>
2021-10-13 14:11:05 +0800

    Forgot password | 13 Oct

--- app/src/main/java/com/example/myapplication/changePassword.java
+++ app/src/main/java/com/example/myapplication/changePassword.java
@@ -1,11 +1,16 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class changePassword extends AppCompatActivity implements View.OnClickListener{
+
+    Button forgotPass;
+
     @Override
     public void onClick(View view) {
 
@@ -15,7 +20,13 @@ public class changePassword extends AppCompatActivity implements View.OnClickLis
         super.onCreate(savedInstanceState);
         setContentView(R.layout.change_password);
 
+        forgotPass = findViewById(R.id.btnForgotPass);
 
-
+        forgotPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(changePassword.this, ForgotPasswordActivity.class));
+            }
+        });
     }
 }
