fc780a4fc7eb856abab46f38fb75213a9d5eaf74
Ong Jia Jun <p20012439@student.newinti.edu.my>
2021-10-26 11:56:47 +0800

    Click In Order

---
+++ app/src/main/java/com/example/myapplication/OrderSummaryActivity.java
@@ -0,0 +1,107 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+import com.example.myapplication.adapter.OrderSummaryAdapter;
+import com.example.myapplication.adapter.ViewOrderAdapter;
+import com.example.myapplication.listener.IOrderSummaryLoadListener;
+import com.example.myapplication.listener.IViewOrderLoadListener;
+import com.example.myapplication.model.OrderSummaryModel;
+import com.example.myapplication.model.ViewOrderModel;
+import com.example.myapplication.utils.SpaceItemDecoration;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class OrderSummaryActivity extends AppCompatActivity implements IOrderSummaryLoadListener {
+
+    @BindView(R.id.recycler_order_summary)
+    RecyclerView recycler_order_summary;
+    @BindView(R.id.orderSummaryLayout)
+    RelativeLayout orderSummaryLayout;
+
+    IOrderSummaryLoadListener orderSummaryLoadListener;
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_summary);
+
+        init();
+        loadOrderFromFirebase();
+    }
+
+    private void loadOrderFromFirebase() {
+        List<OrderSummaryModel> orderSummaryModels = new ArrayList<>();
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://intea-delight-default-rtdb.asia-southeast1.firebasedatabase.app/");
+        DatabaseReference reference = database.getReference("Users")
+                .child(firebaseAuth.getUid()).child("Order");
+
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    for(DataSnapshot orderSummarySnapshot : snapshot.getChildren()){
+                        OrderSummaryModel orderSummaryModel = orderSummarySnapshot.getValue(OrderSummaryModel.class);
+                        orderSummaryModel.setKey(orderSummarySnapshot.getKey());
+                        orderSummaryModels.add(orderSummaryModel);
+                    }
+                    orderSummaryLoadListener.onOrderSummaryLoadSuccess(orderSummaryModels);
+                } else {
+                    orderSummaryLoadListener.onOrderSummaryLoadFailed("No Food Ordered");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                orderSummaryLoadListener.onOrderSummaryLoadFailed(error.getMessage());
+            }
+        });
+    }
+
+    @Override
+    public void onOrderSummaryLoadSuccess(List<OrderSummaryModel> orderSummaryModelList) {
+        OrderSummaryAdapter adapter = new OrderSummaryAdapter(this,orderSummaryModelList, orderSummaryLoadListener);
+        recycler_order_summary.setAdapter(adapter);
+    }
+
+    @Override
+    public void onOrderSummaryLoadFailed(String message) {
+        Snackbar.make(orderSummaryLayout,message,Snackbar.LENGTH_LONG).show();
+    }
+
+    private void init(){
+        ButterKnife.bind(this);
+
+        orderSummaryLoadListener = this;
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
+        recycler_order_summary.setLayoutManager(gridLayoutManager);
+        recycler_order_summary.addItemDecoration(new SpaceItemDecoration());
+    }
+
+
+
+}
+
+
+
+
---
+++ app/src/main/java/com/example/myapplication/model/OrderSummaryModel.java
@@ -0,0 +1,81 @@
+package com.example.myapplication.model;
+
+public class OrderSummaryModel {
+    private String image, name, price, quantity, totalPrice, date, time, orderId, key;
+
+    public OrderSummaryModel(){
+
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(String totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+}
